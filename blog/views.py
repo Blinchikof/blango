3c88f9acf6ce167ac4b3670f85a78f977bd4429e
Blinnykov Igorovych <alexblinnikov19@knu.ua>
2023-01-16 18:40:09 +0000

    Finish caching

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 from blog.models import Post
 from django.shortcuts import render, get_object_or_404
 
+
 from django.shortcuts import redirect
 from blog.forms import CommentForm
 import logging
